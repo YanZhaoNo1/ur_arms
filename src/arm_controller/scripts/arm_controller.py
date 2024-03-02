92fa3b1dd4ba70262f6fc41296afcf7312678091
YanZhao <2293086836@qq.com>
2024-03-02 16:54:41 +0800

    add all joint && yaml

--- src/arm_controller/scripts/arm_controller.py
+++ src/arm_controller/scripts/arm_controller.py
@@ -14,11 +14,31 @@ class armControl:
             '/mov_start', SetBool, self.mov_start)
         self.arm_shoulder_pan_pub = rospy.Publisher(
             "/ur5_arm/shoulder_pan_joint_position_controller/command",Float64,queue_size=1000)
+        self.shoulder_lift_pub = rospy.Publisher(
+            "/ur5_arm/shoulder_lift_joint_position_controller/command",Float64,queue_size=1000)
+        self.elbow_pub = rospy.Publisher(
+            "/ur5_arm/elbow_joint_position_controller/command",Float64,queue_size=1000)
+        self.wrist_1_pub = rospy.Publisher(
+            "/ur5_arm/wrist_1_joint_position_controller/command",Float64,queue_size=1000)
+        self.wrist_2_pub = rospy.Publisher(
+            "/ur5_arm/wrist_2_joint_position_controller/command",Float64,queue_size=1000)
+        self.wrist_3_pub = rospy.Publisher(
+            "/ur5_arm/wrist_3_joint_position_controller/command",Float64,queue_size=1000)
         
         self.arm_shoulder_pan_position = Float64()
+        self.shoulder_lift_position = Float64()
+        self.elbow_position = Float64()        
+        self.wrist_1_position = Float64()
+        self.wrist_2_position = Float64()
+        self.wrist_3_position = Float64()
         self.is_mov_start = False
 
-        self.arm_shoulder_pan_position.data = 3.14
+        self.arm_shoulder_pan_position.data = rospy.get_param("~arm_shoulder_pan_position",3.14)
+        self.shoulder_lift_position.data = rospy.get_param("~shoulder_lift_position",0.0)
+        self.elbow_position.data = rospy.get_param("~elbow_position",0.0)
+        self.wrist_1_position.data = rospy.get_param("~wrist_1_position",3.14)
+        self.wrist_2_position.data = rospy.get_param("~wrist_2_position",0.0)
+        self.wrist_3_position.data = rospy.get_param("~wrist_3_position",0.0)
     
     def mov_start(self, req):
         if not self.is_mov_start:
@@ -39,6 +59,11 @@ class armControl:
     def flow(self):
         if self.is_mov_start == True:
             self.arm_shoulder_pan_pub.publish(self.arm_shoulder_pan_position)
+            self.shoulder_lift_pub.publish(self.shoulder_lift_position)
+            self.elbow_pub.publish(self.elbow_position)
+            self.wrist_1_pub.publish(self.wrist_1_position)
+            self.wrist_2_pub.publish(self.wrist_2_position)
+            self.wrist_3_pub.publish(self.wrist_3_position)
     
     def run(self):
         rate = rospy.Rate(20)
